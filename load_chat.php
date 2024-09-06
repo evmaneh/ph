4f4428075bca960e22b94e6a99d116c87592134e
evelyn <122318862+evmaneh@users.noreply.github.com>
2024-09-06 08:55:20 -0400

    Update load_chat.php

--- load_chat.php
+++ load_chat.php
@@ -1,10 +1,8 @@
 <?php
 if (file_exists('secret/logs/chatlogs.txt')) {
-    // Read the chat logs file into an array, ignoring empty lines
     $chat_logs = file('secret/logs/chatlogs.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     
     foreach ($chat_logs as $log) {
-        // Output each log entry as HTML
         echo "<p>" . $log . "</p>";
     }
 } else {
